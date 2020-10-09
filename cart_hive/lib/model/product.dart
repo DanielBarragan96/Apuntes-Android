e5080a0a7744d6ff50fe16548605353cbd6bec65
DanielBarragan96 <ie702554@iteso.mx>
2020-10-09 07:59:22 -0500

    using flutter packages pub run build_runner build

--- cart_hive/lib/model/product.dart
+++ cart_hive/lib/model/product.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/foundation.dart';
+import 'package:hive/hive.dart';
 
+part 'product.g.dart';
+
+//To generate product.g.dart use command:
+//flutter packages pub run build_runner build
+@HiveType(typeId: 1, adapterName: "CarritoAdapter")
 class Product {
+  @HiveType(typeId: 0)
   final String idProd;
+  @HiveType(typeId: 1)
   final String picture;
+  @HiveType(typeId: 2)
   final String name;
+  @HiveType(typeId: 3)
   final double price;
+  @HiveType(typeId: 4)
   final String descrip;
+  @HiveType(typeId: 5)
   final bool favorite;
+  @HiveType(typeId: 6)
   final int ranking;
+  @HiveType(typeId: 7)
   final int amount;
 
   Product({
