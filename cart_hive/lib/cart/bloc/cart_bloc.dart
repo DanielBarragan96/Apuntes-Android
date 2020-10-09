8f272f4d8c7f1c851bde87e78bb93375c68192a6
DanielBarragan96 <ie702554@iteso.mx>
2020-10-09 08:11:57 -0500

    changed carrito to load data from hive

--- cart_hive/lib/cart/bloc/cart_bloc.dart
+++ cart_hive/lib/cart/bloc/cart_bloc.dart
@@ -4,11 +4,16 @@ import 'package:bloc/bloc.dart';
 import 'package:cart_hive/model/product.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
+import 'package:hive/hive.dart';
+
+import '../../model/product.dart';
 
 part 'cart_event.dart';
 part 'cart_state.dart';
 
 class CartBloc extends Bloc<CartEvent, CartState> {
+  Box _carritoBox = Hive.box("Carrito");
+
   CartBloc() : super(CartInitial());
 
   @override
@@ -16,5 +21,12 @@ class CartBloc extends Bloc<CartEvent, CartState> {
     CartEvent event,
   ) async* {
     // TODO: cargar productos
+    if (event is LoadProductsEvent) {
+      List<Product> _productList = List();
+      for (var i = 0; i < _carritoBox.length; i++) {
+        _productList.add(_carritoBox.get(i) as Product);
+      }
+      yield ElementsLoadedState(prodsList: _productList);
+    }
   }
 }
