682fef7d3b92c032aadaf88b407a7464c639a5f9
DanielBarragan96 <ie702554@iteso.mx>
2020-10-09 07:59:42 -0500

    other changes

--- cart_hive/lib/home/bloc/home_bloc.dart
+++ cart_hive/lib/home/bloc/home_bloc.dart
@@ -5,11 +5,14 @@ import 'package:cart_hive/model/product.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
+import 'package:hive/hive.dart';
 
 part 'home_event.dart';
 part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
+  Box _carritoBox = Hive.box("Carrito");
+
   HomeBloc() : super(HomeInitial());
 
   @override
@@ -18,6 +21,8 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
   ) async* {
     if (event is AddToCartEvent) {
       // TODO: guardar productos en carrito local
+      _carritoBox.add(event.product);
+      yield ElementAddedState();
     }
   }
 }
--- cart_hive/lib/main.dart
+++ cart_hive/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:cart_hive/home/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:hive/hive.dart';
 
 void main() async {
   //init hive
